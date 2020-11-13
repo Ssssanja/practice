6324da08d23a6dfbe4f0bd17c35129b00b8e3f39
alexandrito <7978005@gmail.com>
2020-11-13 23:08:06 +0300

    Cycles 4 done

---
+++ src/Cycles4.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+public class Cycles4 {
+    public static void main(String[] args) {
+        BigInteger result = BigInteger.valueOf(1);
+        for (int i = 1; i<=200; i++){
+            result = result.multiply(BigInteger.valueOf(i*i));
+            System.out.println(result);
+        }
+        System.out.println(result);
+    }
+}
