25d6ccb8a90a60a1d70d2540c9a64a2e27736fe0
alexandrito <7978005@gmail.com>
2020-11-18 21:51:42 +0300

    Arrays 1 done

--- src/Algorithmization/Arrays1.java
+++ src/Algorithmization/Arrays1.java
@@ -10,12 +10,13 @@ public class Arrays1 {
         int result = 0;
 
         int[]a = new int[1000];                 // создание массива
-    for (int i = 1; i<1000; i++){               // заполнение
+        for (int i = 1; i<1000; i++){               // заполнение
         a[i] = i;                               // массива
-    }
+        }
         System.out.println("Введите число К:"); //запрос данного к
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int k = Integer.parseInt(reader.readLine());   // считывание введённого к
+        reader.close();
         for (int x: a){                         // перебор массива
             if (x%k==0){                        // если остаток от деления элемента на к равен нулю
                 result+=x;                      // прибавить элемент к результату
