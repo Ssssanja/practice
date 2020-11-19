4751a2e62bd398e872c2e5a844b3bc35c4ba821a
alexandrito <7978005@gmail.com>
2020-11-20 01:03:08 +0300

    Matrix 6 done

--- src/Algorithmization/Matrix6.java
+++ src/Algorithmization/Matrix6.java
@@ -6,28 +6,39 @@ import java.io.InputStreamReader;
 
 public class Matrix6 {
     public static void main(String[] args) throws IOException {
-        System.out.println("Введите размер таблицы N:");                                //запрос размера матрицы
+        System.out.println("Введите размер таблицы N(Чётное):");                                //запрос размера матрицы
         BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());                                    //считывание с клавиатуры
         reader.close();
 
-        double [][] matrix= new double[n][n];
+        int [][] matrix= new int[n][n];
 
-        for (int i = 0; i<n ; i++){                             //заполнение матрицы по формуле
+        for (int i = 0; i<n ; i++){                             //заполнение матрицы единицами
             for (int j = 0; j<n; j++){
-                matrix[i][j] = Math.sin((Math.sqrt(i)-Math.sqrt(j))/2);
+                matrix[i][j] = 1;
+
             }
-        }
+            if (i>0&&i<n/2){                                    //если строка до n/2
+                for (int l = 0; l<(n-(n-i)); l++){
+                    matrix[i][l] = 0;                           //заполняем от первого до номер строки минус один нулями
+                    matrix[i][n-1-l] = 0;                       //заполняем зеркально нулями в этой строке
+                }
 
-        int count = 0;
-        for (int i = 0; i<n; i++){                                   //подсчёт положительных элементов в матрице
-            for (int j = 0; j<n; j++){
-                if (matrix[i][j]>0){
-                    count++;
+            }
+            else if (i>0){                                      //если строка n/2 или более
+                for (int l = 0; l<(n-1-i); l++){
+                    matrix[i][l] = 0;                           //проворачиваем тот же цикл, только по убывающей
+                    matrix[i][n-1-l] = 0;
                 }
             }
-        }
-        System.out.println("Количество положительных элементов в матрице: " + count);
 
+        }
+
+        for (int i = 0; i<n; i++){                                   //вывод матрицы на печать
+            for (int j = 0; j<n; j++){
+                System.out.print(matrix [i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
