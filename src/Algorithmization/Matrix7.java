00f82433626f50c91e532e8a6a840085d7d9b0fd
alexandrito <7978005@gmail.com>
2020-11-20 01:03:20 +0300

    Matrix 7 done

---
+++ src/Algorithmization/Matrix7.java
@@ -0,0 +1,33 @@
+package Algorithmization;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Matrix7 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Введите размер таблицы N:");                                //запрос размера матрицы
+        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());                                    //считывание с клавиатуры
+        reader.close();
+
+        double [][] matrix= new double[n][n];
+
+        for (int i = 0; i<n ; i++){                             //заполнение матрицы по формуле
+            for (int j = 0; j<n; j++){
+                matrix[i][j] = Math.sin((Math.sqrt(i)-Math.sqrt(j))/2);
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i<n; i++){                                   //подсчёт положительных элементов в матрице
+            for (int j = 0; j<n; j++){
+                if (matrix[i][j]>0){
+                    count++;
+                }
+            }
+        }
+        System.out.println("Количество положительных элементов в матрице: " + count);
+
+    }
+}
