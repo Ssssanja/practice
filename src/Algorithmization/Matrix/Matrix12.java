7b55acadd39102224b80f0cfbab4ebbf791607b3
alexandrito <7978005@gmail.com>
2020-11-21 22:17:05 +0300

    Matrix 12 done

--- src/Algorithmization/Matrix/Matrix12.java
+++ src/Algorithmization/Matrix/Matrix12.java
@@ -54,42 +54,26 @@ public class Matrix12 {
     }
 
     public static int[] reverse(int[]array){                                //метод для разворота строки
-        int[]reversed = new int[array.length];
+        int[]reversed = new int[array.length];                              //создаётся новый массив такого же размера, в который заносятся значения в обратном порядке
         for (int i = array.length-1; i>0;){
             for (int j = 0; j<array.length; j++){
                 reversed[j]=array[i];
                 i--;
             }
         }
-        return reversed;
+        return reversed;                                                    //вернуть обратный массив
     }
 
-    public static void sort(int[]array){                                    //сортировка выбором, взято тут: http://study-java.ru/uroki-java/urok-11-sortirovka-massiva/
-        /*По очереди будем просматривать все подмножества
-      элементов массива (0 - последний, 1-последний,
-      2-последний,...)*/
-        for (int i = 0; i < array.length; i++) {
-        /*Предполагаем, что первый элемент (в каждом
-           подмножестве элементов) является минимальным */
-            int min = array[i];
-            int min_i = i;
-        /*В оставшейся части подмножества ищем элемент,
-           который меньше предположенного минимума*/
-            for (int j = i+1; j < array.length; j++) {
-                //Если находим, запоминаем его индекс
-                if (array[j] < min) {
-                    min = array[j];
-                    min_i = j;
-                }
-            }
-        /*Если нашелся элемент, меньший, чем на текущей позиции,
-          меняем их местами*/
-            if (i != min_i) {
-                int tmp = array[i];
-                array[i] = array[min_i];
-                array[min_i] = tmp;
+    public static void sort(int[]array){                                    //сортировка пузырьком, в два цикла
+        for(int i = array.length-1 ; i > 0 ; i--){                          //внешний цикл, проход с конца к началу
+            for(int j = 0 ; j < i ; j++){                                   //внутренний цикл, с начала к концу
+            if( array[j] > array[j+1] ){                                    //если элемент больше следующего
+                int tmp = array[j];                                         //меняем местами
+                array[j] = array[j+1];
+                array[j+1] = tmp;
             }
         }
+    }
     }
 
 }
